ed2c013a1c305643ab234c785e7593ff8f72ec02
Giorgi <giorgi.janelidze97@gmail.com>
2024-04-10 14:10:33 +0400

    feat: added InMemory DbContext, Platform model and Repository

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,16 @@
+using PlatformService.Models;
+using System.Collections.Generic;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+        
+        IEnumerable<Platform> GetAllPlatforms();
+
+        Platform GetPlatformById(int id);
+
+        void CreatePlatform(Platform platform);
+    }
+}
