bb632b26dd7f722ed1068cbabda5120259673239
Giorgi <giorgi.janelidze97@gmail.com>
2024-04-24 17:00:10 +0400

    feat: added new dtos in commands service

---
+++ Microservices_K8s/CommandsService/Dtos/GenericEventDto.cs
@@ -0,0 +1,7 @@
+namespace CommandsServices.Dtos
+{
+	public class GenericEventDto
+	{
+        public string Event { get; set; }
+    }
+}
--- Microservices_K8s/CommandsService/Models/Platform.cs
+++ Microservices_K8s/CommandsService/Models/Platform.cs
@@ -8,10 +8,13 @@ namespace CommandsService.Models
         [Key]
         [Required]
         public int Id { get; set; }
+
         [Required]
         public int ExternalId { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         public ICollection<Command> Commands { get; set; } = new List<Command>();
     }
 }
