ad2768fbc54b0efb2f45a9ea28f21e71253e4cc1
Giorgi <giorgi.janelidze97@gmail.com>
2024-05-30 17:45:55 +0400

    feat: arranged test filing system

---
+++ Microservices_K8s/Tests/UnitTests/ClassFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tests.UnitTests
+{
+    public class ClassFixture : IDisposable
+    {
+        public int PlatformId = new Random().Next(1, 10);
+        public int CommandId = new Random().Next(1, 10);
+        public ClassFixture() { }
+
+        public void Dispose()
+        {
+        }
+    }
+}
