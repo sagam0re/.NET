7eb990073c2202638e3b661ae920e94b4fb79f0b
Giorgi <giorgi.janelidze97@gmail.com>
2024-08-13 10:24:26 +0400

    feat: added create command handlers

---
+++ CleanArchitecture/HR.LeaveManagement/HR.LeaveManagement.Application/DTOs/LeaveRequestDtos/CreateLeaveRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HR.LeaveManagement.Application.DTOs.Common;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequestDtos
+{
+    public class CreateLeaveRequestDto : BaseDto
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int LeaveTypeId { get; set; }
+        public string RequestedComments { get; set; }
+    }
+}
