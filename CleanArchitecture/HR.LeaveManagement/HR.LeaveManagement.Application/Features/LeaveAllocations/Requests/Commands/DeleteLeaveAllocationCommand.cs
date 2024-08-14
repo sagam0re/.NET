43a740e5f4ef75b5845fa864d946e3f9cfc2d9b2
Giorgi <giorgi.janelidze97@gmail.com>
2024-08-14 10:18:08 +0400

    feat: added delete handlers

---
+++ CleanArchitecture/HR.LeaveManagement/HR.LeaveManagement.Application/Features/LeaveAllocations/Requests/Commands/DeleteLeaveAllocationCommand.cs
@@ -0,0 +1,9 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands
+{
+    public class DeleteLeaveAllocationCommand : BaseDto, IRequest
+    {
+    }
+}
---
+++ CleanArchitecture/HR.LeaveManagement/HR.LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/DeleteLeaveTypeCommand.cs
@@ -0,0 +1,9 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands
+{
+    public class DeleteLeaveTypeCommand : BaseDto, IRequest
+    {
+    }
+}
